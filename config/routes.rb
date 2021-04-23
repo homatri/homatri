3d4cbe7645c327b4ed7736495fc8f803c63c2983
Papa Pathé SENE <PapePathe@users.noreply.github.com>
2021-04-23 17:44:19 -0500

    Setup development dependencies and github action
    We need local dev env setup that allow for clean coding, automated tests
    execution and early smell detection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'accueil#index'
+  root to: 'public_pages/home#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_24_010956) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
 end
