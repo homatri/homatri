2271c0aa0d7018e5706b0ec522d3accf7052a6eb
Papa Pathé SENE <pathe.sene@gmail.com>
2021-05-03 18:14:46 -0500

    Add specs aroud the home pages menu

--- spec/system/homepages_spec.rb
+++ spec/system/homepages_spec.rb
@@ -3,12 +3,52 @@
 require 'system_helper'
 
 RSpec.describe 'Homepage', type: :system do
-  let(:home) { prism.public_pages.load }
+  describe 'public demo pages shows english if locale is forced in url ' do
+    before { visit '/en' }
 
-  describe 'login links' do
     context 'when hospital administrator' do
-      it 'has a link to login page' do
-        expect(home).to have_link('Hopitaux')
+      it 'has a link to hospitals page' do
+        expect(page).to have_link('Hospitals')
+      end
+
+      it 'has a link to town halls page' do
+        expect(page).to have_link('Town Halls')
+      end
+
+      it 'has a link to law courts page' do
+        expect(page).to have_link('Law courts')
+      end
+    end
+  end
+
+  describe 'public demo pages shows french if locale is forced in url ' do
+    before { visit '/fr' }
+
+    context 'when hospital administrator' do
+      it 'has a link to hospitals page' do
+        expect(page).to have_link('Hopitaux')
+      end
+
+      it 'has a link to mayors page' do
+        expect(page).to have_link('Mairies')
+      end
+
+      it 'has a link to law courts page' do
+        expect(page).to have_link('Tribunaux')
+      end
+    end
+  end
+
+  describe 'public demo pages shows french by default' do
+    before { visit root_path }
+
+    context 'when hospital administrator' do
+      it 'has a link to hospitals page' do
+        expect(page).to have_link('Hopitaux')
+      end
+
+      it 'has a link to mayors page' do
+        expect(page).to have_link('Mairies')
       end
     end
   end
---
+++ spec/system/pages/public_pages_prism/home.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PublicPagesPrism
+  # Prism for the home page
+  class Home < SitePrism::Page
+    set_url root_path
+  end
+end
