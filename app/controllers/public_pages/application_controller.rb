0cd097d3e5182612674940f198d6624f078e49cb
Papa Pathé SENE <pathe.sene@gmail.com>
2021-05-05 05:23:49 -0500

    Better homepage

--- app/controllers/public_pages/application_controller.rb
+++ app/controllers/public_pages/application_controller.rb
@@ -4,5 +4,15 @@ module PublicPages
   # Base controller for all public pages
   class ApplicationController < ::ApplicationController
     layout 'public_pages/application'
+
+    before_action :set_language_switcher
+
+    private
+
+    # Initliaze a new instance language switcher
+    def set_language_switcher
+      @language_switcher_component =
+        PublicPages::LanguageSwitcherComponent.new([])
+    end
   end
 end
